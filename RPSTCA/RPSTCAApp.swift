8e79ea76f6f5262cac27a7062506d434eadee13d
mlwelker <nutria.watts.0g@icloud.com>
2023-10-14 10:33:29 -0500

    Initial Commit

---
+++ RPSTCA/RPSTCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  RPSTCAApp.swift
+//  RPSTCA
+//
+//  Created by Michael Welker on 2023-10-14.
+//
+
+import SwiftUI
+
+@main
+struct RPSTCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
